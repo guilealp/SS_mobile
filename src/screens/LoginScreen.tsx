8a5f0acca822bbb407e2f87fcceb4bfa3e78a51c
guilealp <guilherme.3110@hotmail.com>
2024-08-01 09:00:46 -0300

    aula 01/08

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+function Login():React.JSX.Element{
+    return(
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+const styles =StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#93fb9a'
+    }
+})
+export default Login;
